35703f83fa452a176e973dd7d6283f0e824330ea
Sheharyaar <maliksheharyaar@gmail.com>
2022-04-20 03:21:51 +0000

    fix: pass bugfix test case 1

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 import { SenderBubble, OtherUserBubble } from '.';
 import moment from 'moment';
-import { makeStyles } from '@material-ui/core/styles';
+//import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+/*const useStyles = makeStyles(() => ({
   messageDirection: {
     display: 'flex',
     flexDirection:'column-reverse',
     
   },
-}));
+}));*/
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
-  const classes = useStyles();
+  //const classes = useStyles();
+  const reversedMessages = messages.slice(0).reverse();
+  
 
   return (
-    <Box className={classes.messageDirection}>
-      {messages.map((message) => {
+    <Box >
+      {reversedMessages.map((message) => {
         const time = moment(message.createdAt).format('h:mm');
 
         return message.senderId === userId ? (
